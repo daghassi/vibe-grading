1941f95566be26f0a9f4be2e0a7e3c54db656a8f
Lukas Werner <lukas.m.werner@outlook.com>
2025-04-05 14:08:19 -0700

    add icons and fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 module.exports = {
-	mode: "production",
-	entry: {
-		background: "./src/background.js",
-		content: "./src/content.js",
-		options: "./src/options.js",
-		popup: "./src/popup.js",
-	},
-	output: {
-		filename: "[name].js",
-		path: path.resolve(__dirname, "dist"),
-	},
+  mode: "production",
+  entry: {
+    background: "./src/background.js",
+    content: "./src/content.js",
+    options: "./src/options.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
