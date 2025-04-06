42f8f708ab4513357c00e595f535812e899f39df
Daghassi <danial.aghassi@gmail.com>
2025-04-05 18:53:23 -0600

    groq sdk framework

---
+++ src/ai.js
@@ -0,0 +1,21 @@
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function main() {
+  const completion = await groq.chat.completions
+    .create({
+      messages: [
+        {
+          role: "user",
+          content: "Explain the importance of fast language models",
+        },
+      ],
+      model: "llama-3.3-70b-versatile",
+    })
+    .then((chatCompletion) => {
+      console.log(chatCompletion.choices[0]?.message?.content || "");
+    });
+}
+
+main();
